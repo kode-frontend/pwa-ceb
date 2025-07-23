9c0e8e41d7bcf66facb9c8081c6d9d3d4db55c26
Sinkin Semen <sse@kode.ru>
2025-07-23 13:42:22 +0400

    fix route

---
+++ src/pages/NotFoundScreen.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundScreen() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">404 - Not Found</h1>
+      <p className="text-gray-500">Oops! This page does not exist.</p>
+    </div>
+  );
+}
