3f964ed167397caf3f267242d83e1299eae81246
Sinkin Semen <sse@kode.ru>
2025-07-23 18:45:20 +0400

    fix background credit

--- src/pages/CreditsScreen.tsx
+++ src/pages/CreditsScreen.tsx
@@ -1,10 +1,18 @@
 import Overlay from "@components/Content/Overlay";
+import { useTheme } from "@context/ThemeProvider";
+import { colorOptions } from "./ColorSelectionScreen";
 
 export const CreditsScreen = () => {
+  const { theme } = useTheme();
+  const backgroundColor =
+    theme === "standard"
+      ? "#FFFFFF"
+      : colorOptions.find((c) => c.id === theme)?.value || "#316dcc";
   return (
     <div
       className="bg-[#f2f3f5] box-border content-stretch flex flex-col items-start justify-start size-full  pt-[51px] pb-[51px]"
       data-name="кредиты"
+      style={{ backgroundColor }}
     >
       <Overlay />
       <div
